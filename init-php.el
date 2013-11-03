94f32c88fef79646adefa986eecf3f2e8a0d05fa
Maciej Mazur <mamciek@gmail.com>
2013-11-03 22:47:36 +0100

    Set fill column indicator in php-mode

--- init-php.el
+++ init-php.el
@@ -1,8 +1,11 @@
 (require-package 'php-mode)
 (require-package 'php-eldoc)
+(require-package 'fill-column-indicator)
 
 (defun init-php-hook ()
-  (php-eldoc-enable))
+  (php-eldoc-enable)
+  (set-fill-column 120)
+  (turn-on-fci-mode))
 
 (add-hook 'php-mode-hook 'init-php-hook)
 
