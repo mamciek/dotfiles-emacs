7aedc1f2e083678d2b8b2a8dec306dca0f4034c0
Maciej Mazur <mamciek@gmail.com>
2014-11-01 15:42:16 +0100

    Add helm-config as instructed in helm readme

--- init-helm.el
+++ init-helm.el
@@ -2,6 +2,7 @@
 (require-package 'helm-ls-git)
 (require-package 'helm-git-grep)
 
+(require 'helm-config)
 (helm-mode 1)
 (global-set-key (kbd "C-c h") 'helm-mini)
 (global-set-key (kbd "C-x C-i") 'helm-imenu)
