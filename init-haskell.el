ee6f922b4b4c80cbd9e5bfe3c5fa9dc2642254a3
Maciej Mazur <mamciek@gmail.com>
2014-11-01 15:52:21 +0100

    Add init-haskell.el

---
+++ init-haskell.el
@@ -0,0 +1,11 @@
+(require-package 'haskell-mode)
+
+(add-hook 'haskell-mode-hook 'turn-on-haskell-indentation)
+(add-hook 'haskell-mode-hook 'turn-on-haskell-doc-mode)
+
+(eval-after-load "haskell-mode"
+  '(progn
+     (define-key haskell-mode-map (kbd "C-,") 'haskell-move-nested-left)
+     (define-key haskell-mode-map (kbd "C-.") 'haskell-move-nested-right)))
+
+(provide 'init-haskell)
