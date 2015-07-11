2f72816571985cead5b0325025b64d7953a6fbe7
Maciej Mazur <mamciek@gmail.com>
2015-07-11 16:55:11 +0200

    Upgrade to magit 2

--- home/.emacs.d/lisp/init-magit.el
+++ home/.emacs.d/lisp/init-magit.el
@@ -1,7 +1,6 @@
 (require-package 'magit)
 
-(global-set-key (kbd "C-c g") 'magit-status)
-
-(setq magit-last-seen-setup-instructions "1.4.0")
+(global-set-key (kbd "C-x g") 'magit-status)
+(global-set-key (kbd "C-x M-g") 'magit-dispatch-popup)
 
 (provide 'init-magit)
