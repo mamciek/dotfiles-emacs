76beef49e764d45964295bff45dbb0d57f4a4148
Maciej Mazur <mamciek@gmail.com>
2017-03-05 17:10:55 +0100

    Remove marmalade repo

--- init.el
+++ init.el
@@ -6,8 +6,8 @@
 	     '("melpa" . "http://melpa.org/packages/") t)
 (add-to-list 'package-archives
 	     '("org" . "http://orgmode.org/elpa/") t)
-(add-to-list 'package-archives
-	     '("marmalade" . "http://marmalade-repo.org/packages/") t)
+;; (add-to-list 'package-archives
+;; 	     '("marmalade" . "http://marmalade-repo.org/packages/") t)
 
 (package-initialize)
 
