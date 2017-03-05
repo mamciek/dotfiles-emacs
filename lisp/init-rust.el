927e0469032f87fdf1f65c1151b38b404bc39830
Maciej Mazur <mamciek@gmail.com>
2017-03-05 21:21:55 +0100

    update rust config

--- lisp/init-rust.el
+++ lisp/init-rust.el
@@ -1,6 +1,26 @@
-(require-package 'rust-mode)
-(require-package 'flycheck-rust)
-(require-package 'toml-mode)
+(use-package rust-mode
+  :ensure t)
 
+(use-package flycheck-rust
+  :ensure t
+  :init
+  (add-hook 'flycheck-mode-hook #'flycheck-rust-setup))
+
+(use-package cargo
+  :ensure t
+  :config
+  (add-hook 'rust-mode-hook 'cargo-minor-mode))
+
+(use-package racer
+  :ensure t
+  :bind ("TAB" . company-indent-or-complete-common)
+  :init
+  (add-hook 'rust-mode-hook #'racer-mode)
+  (add-hook 'racer-mode-hook #'eldoc-mode)
+;  (define-key rust-mode-map (kbd "TAB") #'company-indent-or-complete-common)
+  )
+
+(use-package toml-mode
+  :ensure t)
 
 (provide 'init-rust)
